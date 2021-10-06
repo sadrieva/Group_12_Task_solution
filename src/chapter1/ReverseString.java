a553d37ec09cf72da0c7dd3e47e1776969ed2aad
Shirin Geldiyeva <shirin7271@gmail.com>
2021-10-05 22:39:18 -0400

    Start adding reverse method

--- src/chapter1/ReverseString.java
+++ src/chapter1/ReverseString.java
@@ -1,8 +1,14 @@
 package chapter1;
 
+import java.util.Scanner;
+
 public class ReverseString {
     public static void main(String[] args) {
         //Some text
+        Scanner inp = new Scanner(System.in);
+        String reverse = "";
+        System.out.println("Type some words");
+        String word = inp.nextLine();
 
 
 
